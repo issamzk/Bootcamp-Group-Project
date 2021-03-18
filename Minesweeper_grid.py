b02d0339ff513e5f947520bce965955b8cc4c266
Kagiso <66702678+kaygee-sp@users.noreply.github.com>
2021-03-18 17:58:30 +0200

    Add files via upload

--- Minesweeper_grid.py
+++ Minesweeper_grid.py
@@ -8,20 +8,23 @@ def board (rows_columns, bombs_amount):
 
 begin = False
 while begin == False:
-    rows_columns = int(input("\nHow many rows and columns would you like[min = 4]: "))
-    bombs_amount = int(input(f"How many bombs would you like on the board[min = 1 | max < {rows_columns}: "))
+    try:
+        rows_columns = int(input("\nHow many rows and columns would you like[min = 4]: "))
+        bombs_amount = int(input(f"How many bombs would you like on the board[min = 1 | max < {rows_columns}: "))
 
-    if rows_columns < 4 or rows_columns < 0:
-        print("\nThe minimum amount of rows and columns must be greater than 4")
-        continue
-    elif bombs_amount <= 0:
-        print("\nThe minimum amount of bombs must be greater than 0")
-        continue
-    elif bombs_amount >= rows_columns:
-        print(f"\nThe maximum amount of bombs must be less than {rows_columns}")
-        continue
-    else:
-        begin = True
+        if rows_columns < 4 or rows_columns < 0:
+            print("\nThe minimum amount of rows and columns must be greater than 4")
+            continue
+        elif bombs_amount <= 0:
+            print("\nThe minimum amount of bombs must be greater than 0")
+            continue
+        elif bombs_amount >= rows_columns:
+            print(f"\nThe maximum amount of bombs must be less than {rows_columns}")
+            continue
+        else:
+            begin = True
+    except ValueError:
+        print("Please enter a valid integer.")
 
 print("\n\n\n") 
 
